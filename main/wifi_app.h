24eed176d80b4bbbb0d55e42fadd55e0707b9322
Ilija <tomic.ilija305@gmail.com>
2024-02-15 12:05:35 +0100

    wifi_app.h updated

--- main/wifi_app.h
+++ main/wifi_app.h
@@ -4,9 +4,55 @@
 #include "esp_netif.h"
 
 // WiFi application settings
-#define WIFI_AP_SSID                "ESP32_AP"  // AP name [AP - Access Point]
-#define WIFI_AP_PASSWORD            "password"  // AP password
-#define 
+#define WIFI_AP_SSID                "ESP32_AP"      // AP name [AP - Access Point]
+#define WIFI_AP_PASSWORD            "password"      // AP password
+#define WIFI_AP_CHANNEL             1               // AP channel
+#define WIFI_AP_SSID_HIDDEN         0               // AP visibility
+#define WIFI_AP_MAX_CONNECTION      5               // AP max clients
+#define WIFI_AP_BEACON_INTERVAL     100             // AP beacons: 100 milliseconds recommended
+#define WIFI_AP_IP                  "192.168.0.1"   // AP default IP
+#define WIFI_AP_GATEWAY             "192.168.0.1"   // AP default Gateway (should be the same as the IP)
+#define WIFI_AP_NETMASK             "255.255.255.0" // AP netmask
+#define WIFI_AP_BANDWIDTH           WIFI_BW_HT20    // AP bandwidth 20 MHz (40MHz is the other option)
+#define WIFI_AP_POWER_SAVE          WIFI_PS_NONE    // Power save not used  
+#define MAX_SSID_LENGTH             32              // IEEE standard maximum
+#define MAX_PASSWORD_LENGTH         64              // IEEE standard maximum
+#define MAX_CONNECTION_RETRIES      5               // Retry number on disconnect
 
+// netif object for the Station and Access Point
+extern esp_netif_t* esp_netif_sta;
+extern esp_netif_t* esp_netif_ap;
+
+/**
+ * Message IDs for the WiFi application task
+ * @note Expand this based on your application requirements
+*/
+
+typedef enum wifi_app_message{
+    WIFI_APP_MSG_START_HTTP_SERVER = 0,
+    WIFI_APP_MSG_CONNECTING_FROM_HTTP_SERVER,
+    WIFI_APP_MSG_STA_CONNECTED_GOT_IP,
+} wifi_app_message_e;
+
+/**
+ * Structure for the message queue
+ * @note Exapnd this based on application requirements e.g. add another type and parameter as required
+*/
+typedef struct wifi_app_queue_message{
+    wifi_app_message_e SSID;
+} wifi_app_queue_message_t;
+
+/**
+ * Sends a message to the queue
+ * @param msgID message ID from the wifi_app_message_e enum.
+ * @return pdTRUE if an item was successfully sent to the queue, otherwise pdFALSE.
+ * @note Exapnd the parameter list based on your requirements e.g. how you've expanded the wifi_app_queue_message_t
+*/
+BaseType_t wifi_app_send_message(wifi_app_message_e msgID);
+
+/**
+ * Starts the WiFi RTOS task
+*/
+void wifi_app_start(void);
 
 #endif /* MAIN_WIFI_APP_H_ */
