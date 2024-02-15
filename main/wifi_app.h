14de05016fb7f8a4e8787f0d3705aaacd3ae58e0
Ilija <tomic.ilija305@gmail.com>
2024-02-15 11:37:13 +0100

    Update

---
+++ main/wifi_app.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_WIFI_APP_H_
+#define MAIN_WIFI_APP_H_
+
+#include "esp_netif.h"
+
+// WiFi application settings
+#define WIFI_AP_SSID                "ESP32_AP"  // AP name [AP - Access Point]
+#define WIFI_AP_PASSWORD            "password"  // AP password
+#define 
+
+
+#endif /* MAIN_WIFI_APP_H_ */
