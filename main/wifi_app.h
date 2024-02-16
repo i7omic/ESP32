d1cea86dc95351b5f99d79518c5ec6ace29cd6bc
Ilija <tomic.ilija305@gmail.com>
2024-02-16 13:43:34 +0100

    new update

--- main/wifi_app.h
+++ main/wifi_app.h
@@ -2,19 +2,22 @@
 #define MAIN_WIFI_APP_H_
 
 #include "esp_netif.h"
+#include "esp_netif_types.h"
+#include "freertos/FreeRTOS.h"
+#include "esp_wifi_types.h"
 
 // WiFi application settings
 #define WIFI_AP_SSID                "ESP32_AP"      // AP name [AP - Access Point]
 #define WIFI_AP_PASSWORD            "password"      // AP password
 #define WIFI_AP_CHANNEL             1               // AP channel
 #define WIFI_AP_SSID_HIDDEN         0               // AP visibility
-#define WIFI_AP_MAX_CONNECTION      5               // AP max clients
+#define WIFI_AP_MAX_CONNECTIONS      5               // AP max clients
 #define WIFI_AP_BEACON_INTERVAL     100             // AP beacons: 100 milliseconds recommended
 #define WIFI_AP_IP                  "192.168.0.1"   // AP default IP
 #define WIFI_AP_GATEWAY             "192.168.0.1"   // AP default Gateway (should be the same as the IP)
 #define WIFI_AP_NETMASK             "255.255.255.0" // AP netmask
 #define WIFI_AP_BANDWIDTH           WIFI_BW_HT20    // AP bandwidth 20 MHz (40MHz is the other option)
-#define WIFI_AP_POWER_SAVE          WIFI_PS_NONE    // Power save not used  
+#define WIFI_STA_POWER_SAVE          WIFI_PS_NONE    // Power save not used  
 #define MAX_SSID_LENGTH             32              // IEEE standard maximum
 #define MAX_PASSWORD_LENGTH         64              // IEEE standard maximum
 #define MAX_CONNECTION_RETRIES      5               // Retry number on disconnect
@@ -32,6 +35,7 @@ typedef enum wifi_app_message{
     WIFI_APP_MSG_START_HTTP_SERVER = 0,
     WIFI_APP_MSG_CONNECTING_FROM_HTTP_SERVER,
     WIFI_APP_MSG_STA_CONNECTED_GOT_IP,
+    WIFI_APP_MSG_STA_DISCONNECTED,
 } wifi_app_message_e;
 
 /**
@@ -39,7 +43,7 @@ typedef enum wifi_app_message{
  * @note Exapnd this based on application requirements e.g. add another type and parameter as required
 */
 typedef struct wifi_app_queue_message{
-    wifi_app_message_e SSID;
+    wifi_app_message_e msgID;
 } wifi_app_queue_message_t;
 
 /**
@@ -54,5 +58,10 @@ BaseType_t wifi_app_send_message(wifi_app_message_e msgID);
  * Starts the WiFi RTOS task
 */
 void wifi_app_start(void);
+
+/**
+ * Gets the wifi configuration
+*/
+wifi_config_t* wifi_app_get_wifi_config(void);
 
 #endif /* MAIN_WIFI_APP_H_ */
